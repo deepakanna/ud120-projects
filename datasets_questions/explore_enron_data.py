20348765dfe30aa1c8894b39760ccd9e3e9fc028
deepakanna <110763030+deepakanna@users.noreply.github.com>
2022-08-20 09:12:07 +0530

    updated

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,23 +1,1 @@
-#!/usr/bin/python3
-
-""" 
-    Starter code for exploring the Enron dataset (emails + finances);
-    loads up the dataset (pickled dict of dicts).
-
-    The dataset has the form:
-    enron_data["LASTNAME FIRSTNAME MIDDLEINITIAL"] = { features_dict }
-
-    {features_dict} is a dictionary of features associated with that person.
-    You should explore features_dict as part of the mini-project,
-    but here's an example to get you started:
-
-    enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
-"""
-#import pickle
-
-#enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
-import joblib
-
-enron_data = joblib.load(open("../final_project/final_project_dataset.pkl", "rb"))
-
+#!/usr/bin/python3"""     Starter code for exploring the Enron dataset (emails + finances);    loads up the dataset (pickled dict of dicts).    The dataset has the form:    enron_data["LASTNAME FIRSTNAME MIDDLEINITIAL"] = { features_dict }    {features_dict} is a dictionary of features associated with that person.    You should explore features_dict as part of the mini-project,    but here's an example to get you started:    enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000    """import pickleenron_data = pickle.load(open("../final_project/final_project_dataset_unix.pkl", "rb"))#import joblib#enron_data = joblib.load(open("../final_project/final_project_dataset.pkl", "rb"))#file_path = "../final_project/final_project_dataset_modified.pkl"#enron_data = pickle.load((open(file_path, "rb")), fix_imports=True)
